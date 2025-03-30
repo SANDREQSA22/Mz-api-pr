67cbcd98fcf3caf4688b2cc3fa81f9cf1939a154
SANDREQSA22 <sandrokupatadze2009@gmail.com>
2025-03-30 15:08:52 +0400

    jwtauth

--- library/api.py
+++ library/api.py
@@ -6,9 +6,43 @@ from typing import List, Dict, Optional
 
 router = Router()
 
+from django.contrib.auth import authenticate
+from datetime import datetime
+from rest_framework_simplejwt.tokens import RefreshToken
+
+#-----AUTH ROUTER-----#
+
+auth_router = Router()
+
+@auth_router.post("/token")
+def get_token(request, username: str, password: str):
+    user = authenticate(username=username, password=password)
+
+    if user:
+        refresh = RefreshToken.for_user(user)
+        return {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
+    return {"error": "Invalid credentials"}, 401
+
+from ninja.security import HttpBearer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+class JWTBearer(HttpBearer):
+    def authenticate(self, request, token):
+        try:
+            auth = JWTAuthentication()
+            validated_token = auth.get_validated_token(token)
+            user = auth.get_user(validated_token)
+            return user
+        except Exception as e:
+            print(f"Authentication failed: {e}")
+            return None
+
 # --------------------- BOOKS API ---------------------
 
-@router.get("/books", response=List[Dict])
+@router.get("/books", response=List[Dict], auth=JWTBearer())
 def get_books(request, genre: Optional[str] = None, borrowed_by: Optional[str] = None, borrowed: Optional[bool] = None):
     qs = Book.objects.all()
 
